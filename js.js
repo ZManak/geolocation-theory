5a79a0ce6b392e0f0a4f4462d2b0b345a7dac7db
ZManak <javifuer@gmail.com>
2022-12-19 11:50:25 +0100

    added/Map

---
+++ js.js
@@ -0,0 +1,55 @@
+//the crucial data for geolocation are coordinates
+//these can be obtained manually or fetched from an API
+
+//Geolocation.getCurrentPosition()
+//returns the current coordinates
+
+//Object.navigator()
+
+//golocation.watchPosition() => tracks the location
+
+//-------------------------------------------------------------------
+
+/* Leafletjs.com - Geolocation API
+const leaflet = "https://leafletjs.com/";
+
+let map = L.map('map').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+L.marker([51.5, -0.09]).addTo(map)
+    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+    .openPopup();*/
+
+//Demo
+
+//Pintar Mapa
+let map = L.map('map').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+//Añadir elementos
+let marker = L.marker([51.5, -0.09]).addTo(map);
+
+var circle = L.circle([51.508, -0.11], {
+    color: 'red',
+    fillColor: '#f03',
+    fillOpacity: 0.5,
+    radius: 500
+}).addTo(map);
+
+var polygon = L.polygon([
+    [51.509, -0.08],
+    [51.503, -0.06],
+    [51.51, -0.047]
+]).addTo(map);
+
+marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+circle.bindPopup("I am a circle.");
+polygon.bindPopup("I am a polygon.");
+
